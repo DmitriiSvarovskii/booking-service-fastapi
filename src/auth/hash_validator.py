8cfbf8fecdace0af5ab4d1961062c8c66530bde2
Dmitrii <swarovskidima@gmail.com>
2025-03-14 02:47:21 +0700

    Рефакторинг логики валидации данных в отдельный класс DataValidator

--- src/auth/hash_validator.py
+++ src/auth/hash_validator.py
@@ -3,35 +3,39 @@ import hashlib
 
 from urllib.parse import unquote
 
-from src.configs.app import settings
 
+class DataValidator:
+    def __init__(self, bot_token: str, secret_key_str: str):
+        self.bot_token = bot_token
+        self.secret_key_str = secret_key_str
 
-def is_valid_data(init_data: str) -> bool:
-    init_data = unquote(init_data)
+    def is_valid_data(self, init_data: str) -> bool:
+        """Проверка валидности данных."""
 
-    params = dict(chunk.split("=") for chunk in init_data.split("&"))
+        init_data = unquote(init_data)
+        params = dict(chunk.split("=") for chunk in init_data.split("&"))
 
-    my_hash = params.get("hash")
+        my_hash = params.get("hash")
 
-    if not my_hash:
-        print("Hash not found!")
-        return False
+        if not my_hash:
+            print("Hash not found!")
+            return False
 
-    params.pop("hash", None)
+        params.pop("hash", None)
 
-    init_data_to_check = "\n".join(
-        [f"{key}={value}" for key, value in sorted(params.items())])
+        init_data_to_check = "\n".join(
+            [f"{key}={value}" for key, value in sorted(params.items())])
 
-    secret_key = hmac.new(
-        settings.SECRET_KEY_STR.encode(),
-        settings.BOT_TOKEN.encode(),
-        hashlib.sha256
-    ).digest()
+        secret_key = hmac.new(
+            self.secret_key_str.encode(),
+            self.bot_token.encode(),
+            hashlib.sha256
+        ).digest()
 
-    data_check = hmac.new(
-        secret_key,
-        init_data_to_check.encode(),
-        hashlib.sha256
-    ).hexdigest()
+        data_check = hmac.new(
+            secret_key,
+            init_data_to_check.encode(),
+            hashlib.sha256
+        ).hexdigest()
 
-    return data_check == my_hash
+        return data_check == my_hash
--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, HTTPException, status
 
-from src.auth.hash_validator import is_valid_data
+from src.auth.hash_validator import DataValidator
 from src.schemas.auth import InitDataRequest
+from src.configs.app import settings
 
 router = APIRouter(
     prefix="/api/v1/auth",
@@ -12,7 +13,12 @@ router = APIRouter(
 @router.post("/validate_data/", status_code=status.HTTP_200_OK)
 async def validate_data(request: InitDataRequest):
     try:
-        if is_valid_data(request.init_data):
+        validator = DataValidator(
+            bot_token=settings.BOT_TOKEN,
+            secret_key_str=settings.SECRET_KEY_STR
+        )
+
+        if validator.is_valid_data(request.init_data):
             return {"message": "Data is from Telegram"}
         else:
             raise HTTPException(status_code=400, detail="Data is not valid")
